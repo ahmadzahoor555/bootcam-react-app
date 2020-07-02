1f855692e290ebb2a6ced1d684aa9f19934cb134
ahmadzahoor555 <44691924+ahmadzahoor555@users.noreply.github.com>
2020-07-02 17:49:44 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React from Zahoor Ahmad
+          Learn React from Zahoor Ahmad Karime
         </a>
       </header>
     </div>
